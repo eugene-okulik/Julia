c8f58a446ce3958aebdbcc9332e3dc69769d1f77
Julia Shevkunova <shevkunova.yulia@gmail.com>
2024-03-17 19:20:56 +0100

    part of the task completed

---
+++ homework/julia_shevkunova/homework_3/ex_1.py
@@ -0,0 +1,14 @@
+my_dict = {'tuple': '(1, 2, 3, 4, 5)',
+           'list': '[list1, 4, None, 23, False]',
+           'set': '{1, 4, 65, testa, 67}',
+           'ddict': {'code': 24737, 'title': "Game Designer",
+           'town': 'Minsk', 'country': 'Belarus', 'Work': 'remote'}}
+
+
+
+my_dict['i am a tuple'] = 'add'  # добавила элемент с ключом ('i am a tuple',) и любым значением в словарь
+my_dict['set'] = 'new_elem' #  добавила новый элемент в множество
+print(my_dict) # вывела словарь на экран
+
+
+

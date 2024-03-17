ddf5e540139e7ec34963febdecb5b1d00a02b70a
Julia Shevkunova <shevkunova.yulia@gmail.com>
2024-03-17 19:44:11 +0100

    homework 3 updated

--- homework/julia_shevkunova/homework_3/ex_1.py
+++ homework/julia_shevkunova/homework_3/ex_1.py
@@ -1,14 +1,10 @@
-my_dict = {'tuple': '(1, 2, 3, 4, 5)',
-           'list': '[list1, 4, None, 23, False]',
-           'set': '{1, 4, 65, testa, 67}',
-           'ddict': {'code': 24737, 'title': "Game Designer",
-           'town': 'Minsk', 'country': 'Belarus', 'Work': 'remote'}}
+my_dict = {'tuple': '(1, 2, 3, 4, 5)', 'list': '[list1, 4, None, 23, False]', 'set': 'new_elem',
+           'ddict': {'code': 24737, 'title': "Game Designer", 'town': 'Minsk',
+                     'country': 'Belarus', 'Work': 'remote'}, 'i_am_a_tuple': 'add', 'I_am_a_tuple': 'add'}
 
+print(my_dict)
 
+my_dict['I_am_a_tuple'] = 'add'
 
-my_dict['i am a tuple'] = 'add'  # добавила элемент с ключом ('i am a tuple',) и любым значением в словарь
-my_dict['set'] = 'new_elem' #  добавила новый элемент в множество
-print(my_dict) # вывела словарь на экран
-
-
+my_dict['set'] = 'new_elem'
 

538ec988aed3c9d433b475a9805d2e3bc3508e9e
Julia Shevkunova <shevkunova.yulia@gmail.com>
2024-03-18 17:47:17 +0100

    homework 3 updated

--- homework/julia_shevkunova/homework_3/ex_1.py
+++ homework/julia_shevkunova/homework_3/ex_1.py
@@ -1,9 +1,14 @@
-my_dict = {'tuple': '(1, 2, 3, 4, 5)', 'list': '[list1, 4, None, 23, False]', 'set': 'new_elem',
+my_dict = {'tuple': '1, 2, 3, 4, 5', 'list': ['list1', 4, None, 23, False], 'set': {'new_elem', 23, 34, False, None},
            'ddict': {'code': 24737, 'title': "Game Designer", 'town': 'Minsk',
-                     'country': 'Belarus', 'Work': 'remote'}, 'i_am_a_tuple': 'add', 'I_am_a_tuple': 'add'}
+                     'country': 'Belarus', 'Work': 'remote'}}
 
-print(my_dict)
 
-my_dict['I_am_a_tuple'] = 'add'
+print(my_dict['tuple'][-1]) # вывод на экран последнего элмента tuple
+my_dict['list'].append(8)  # добавление элмента в конец списка
+my_dict['list'].pop(1) # удаление второго элмента списка
+my_dict['ddict']['I_am_a_tuple'] = 'add' # добавление элмента с ключем в словарь
+my_dict['ddict'].pop('code') # удаление элмента из словаря
+my_dict['set'].add('text') # добавление элемента в множество
+my_dict['set'].remove(23)  # удаление элемента из множества
+print(my_dict) # вывол на экран словаря
 
-my_dict['set'] = 'new_elem'

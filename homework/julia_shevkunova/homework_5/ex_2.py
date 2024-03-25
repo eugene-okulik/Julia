e891fad7f9fc8bab30fd450563b3765af7360462
Julia Shevkunova <shevkunova.yulia@gmail.com>
2024-03-25 15:56:08 +0100

    homework 5 update

--- homework/julia_shevkunova/homework_5/ex_2.py
+++ homework/julia_shevkunova/homework_5/ex_2.py
@@ -2,12 +2,17 @@ string_1 = 'результат операции: 42'
 string_2 = 'результат операции: 514'
 string_3 = 'результат работы программы: 9'
 
-print(string_1.index('42'))
-print(string_2.index('514'))
-print(string_3.index('9'))
+# print(string_1.index('42'))
+# print(string_2.index('514'))
+# print(string_3.index('9'))
 
-num = 10
+# print(int(string_1[20:]) + num)
+# print(int(string_2[20:]) + num)
+# print(int(string_3[27:]) + num)
 
-print(int(string_1[20:]) + num)
-print(int(string_2[20:]) + num)
-print(int(string_3[27:]) + num)
+print(string_1.index(':'))
+print(string_3.index(':'))
+
+print(int(string_1[19:]) + 10)
+print(int(string_2[19:]) + 10)
+print(int(string_3[27:]) + 10)

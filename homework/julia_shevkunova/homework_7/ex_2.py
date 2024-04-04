025bbb72495d9149a619b8229d208cd7c0f4802e
Julia Shevkunova <shevkunova.yulia@gmail.com>
2024-04-04 16:10:51 +0200

    homework 7 updated

--- homework/julia_shevkunova/homework_7/ex_2.py
+++ homework/julia_shevkunova/homework_7/ex_2.py
@@ -1,19 +1,10 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
-a, b, c, d = words
+# a, b, c, d = words
 
 
 def print_keу(words):
-    if words == a:
-        print(words * 3)
-    elif words == b:
-        print(words * 5)
-    elif words == c:
-        print(words * 1)
-    elif words == d:
-        print(words * 50)
+    for word, count in words.items():
+        print(word * count)
 
 
-print_keу(a)
-print_keу(b)
-print_keу(c)
-print_keу(d)
+print_keу(words)

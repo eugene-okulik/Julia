68855d207f877ab0a648754a2f0f30cec41a0ba8
Julia Shevkunova <shevkunova.yulia@gmail.com>
2024-07-31 21:03:01 +0300

    homework 11 updated

--- homework/julia_shevkunova/homework_11/ex_1.py
+++ homework/julia_shevkunova/homework_11/ex_1.py
@@ -16,21 +16,22 @@ book_3 = Book('Мир животных', 'Лизанков', 433, '3745JF', Fals
 book_4 = Book('Сказки добрых людей', 'Чеченко', 433, '375FD', False)
 book_5 = Book('Здоровое питание', 'Шебашкович', 123, '1245FD', False)
 
-print('Название: ', book_1.title, '|', 'Автор: ', book_1.author, '|', 'Страниц: ', book_1.number_of_pages, '|',
-      'ISBN: ', book_1.ISBN, '|', 'Текст: ', book_1.text, '|', 'Материал: ', book_1.page_material, '|',
-      'Зарезервировано: ', book_1.reserved)
-print('Название: ', book_2.title, '|', 'Автор: ', book_2.author, '|', 'Страниц: ', book_2.number_of_pages, '|',
-      'ISBN: ', book_2.ISBN, '|', 'Текст: ', book_2.text, '|', 'Материал: ', book_2.page_material, '|',
-      'Зарезервировано: ', book_2.reserved)
-print('Название: ', book_3.title, '|', 'Автор: ', book_3.author, '|', 'Страниц: ', book_3.number_of_pages, '|',
-      'ISBN: ', book_3.ISBN, '|', 'Текст: ', book_3.text, '|', 'Материал: ', book_3.page_material, '|',
-      'Зарезервировано: ', book_3.reserved)
-print('Название: ', book_4.title, '|', 'Автор: ', book_4.author, '|', 'Страниц: ', book_4.number_of_pages, '|',
-      'ISBN: ', book_4.ISBN, '|', 'Текст: ', book_4.text, '|', 'Материал: ', book_4.page_material, '|',
-      'Зарезервировано: ', book_4.reserved)
-print('Название: ', book_5.title, '|', 'Автор: ', book_5.author, '|', 'Страниц: ', book_5.number_of_pages, '|',
-      'ISBN: ', book_5.ISBN, '|', 'Текст: ', book_5.text, '|', 'Материал: ', book_5.page_material, '|',
-      'Зарезервировано: ', book_5.reserved)
+
+def descr_of_books(book):
+    if book.reserved is True:
+        print('Название: ', book.title, '|', 'Автор: ', book.author, '|', 'Страниц: ', book.number_of_pages, '|',
+              'ISBN: ', book.ISBN, '|', 'Текст: ', book.text, '|', 'Материал: ', book.page_material, '|',
+              'Зарезервировано: ', book.reserved)
+    else:
+        print('Название: ', book.title, '|', 'Автор: ', book.author, '|', 'Страниц: ', book.number_of_pages, '|',
+              'ISBN: ', book.ISBN, '|', 'Текст: ', book.text, '|', 'Материал: ', book.page_material)
+
+
+descr_of_books(book_1)
+descr_of_books(book_2)
+descr_of_books(book_3)
+descr_of_books(book_4)
+descr_of_books(book_5)
 
 
 class SchoolBook(Book):
@@ -45,14 +46,19 @@ class SchoolBook(Book):
 math_book = SchoolBook('Алгебра', 'Кончаковский', 234, '3787D', False, 'Математика', 5, True)
 physics_book = SchoolBook('Оптика', 'Петруня', 222, '3737D', True, 'Физика', 5, True)
 
-print('Название: ', math_book.title, '|', 'Автор: ', math_book.author, '|', 'Страниц: ', math_book.number_of_pages, '|',
-      'ISBN: ', math_book.ISBN, '|', 'Текст: ', math_book.text, '|', 'Материал: ', math_book.page_material, '|',
-      'Зарезервировано: ', math_book.reserved, '|', 'Предмет: ', math_book.subject, '|', 'Класс: ',
-      math_book.school_class, '|', 'Наличие заданий: ', math_book.presence_of_tasks)
 
-print('Название: ', physics_book.title, '|', 'Автор: ', physics_book.author, '|', 'Страниц: ',
-      physics_book.number_of_pages, '|',
-      'ISBN: ', physics_book.ISBN, '|', 'Текст: ', physics_book.text, '|', 'Материал: ', physics_book.page_material,
-      '|',
-      'Зарезервировано: ', physics_book.reserved, '|', 'Предмет: ', physics_book.subject, '|', 'Класс: ',
-      physics_book.school_class, '|', 'Наличие заданий: ', physics_book.presence_of_tasks)
+def descr_of_books(book):
+    if book.reserved is True:
+        print('Название: ', book.title, '|', 'Автор: ', book.author, '|', 'Страниц: ', book.number_of_pages, '|',
+              'ISBN: ', book.ISBN, '|', 'Текст: ', book.text, '|', 'Материал: ', book.page_material, '|',
+              'Зарезервировано: ', book.reserved, '|', 'Предмет: ', book.subject, '|', 'Класс: ',
+              book.school_class, '|', 'Наличие заданий: ', book.presence_of_tasks)
+    else:
+        print('Название: ', book.title, '|', 'Автор: ', book.author, '|', 'Страниц: ', book.number_of_pages, '|',
+              'ISBN: ', book.ISBN, '|', 'Текст: ', book.text, '|', 'Материал: ', book.page_material, '|', 'Предмет: ',
+              book.subject, '|', 'Класс: ',
+              book.school_class, '|', 'Наличие заданий: ', book.presence_of_tasks)
+
+
+descr_of_books(physics_book)
+descr_of_books(math_book)

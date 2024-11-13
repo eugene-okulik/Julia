87e132b0475d47f1e7ce4359c48fbcb083e0b5d2
Julia Shevkunova <shevkunova.yulia@gmail.com>
2024-11-13 18:37:22 +0300

    julia_s homework 12

--- homework/julia_shevkunova/homework_12/ex.1.py
+++ homework/julia_shevkunova/homework_12/ex.1.py
@@ -6,20 +6,16 @@ class Flowers:
         self.freshness = freshness
         self.color = color
 
-
 class Rose(Flowers):
     def __init__(self, price, time_of_life, freshness, color):
         super().__init__(price, time_of_life, freshness, color)
 
-
 early_rose = Rose(15, 5, freshness=1, color='white')
 late_rose = Rose(16, 10, freshness=2, color='red')
-
 
 class Lotus(Flowers):
     def __init__(self, price, time_of_life, freshness, color):
         super().__init__(price, time_of_life, freshness, color)
-
 
 early_lotus = Lotus(11, 5, freshness=4, color='white')
 late_lotus = Lotus(17, 7, freshness=5, color='yellow')
@@ -27,7 +23,6 @@ late_lotus = Lotus(17, 7, freshness=5, color='yellow')
 list_of_flowers = [early_rose, late_rose, early_lotus, late_lotus]
 total_lifetime = early_rose.time_of_life + late_rose.time_of_life + early_lotus.time_of_life + late_lotus.time_of_life
 number_of_list_of_flowers = len(list_of_flowers)
-
 
 class CollectBouquet:
     def __init(self, list_flowers):
@@ -42,7 +37,7 @@ class CollectBouquet:
     @staticmethod
     def time_of_fading():
         time_fading = total_lifetime / number_of_list_of_flowers
-        print(time_fading)
+        return time_fading
 
     @staticmethod
     def sort_by_price():
@@ -66,5 +61,4 @@ class CollectBouquet:
         lifetime = [(early_rose.time_of_life + late_rose.time_of_life +
                      early_lotus.time_of_life + late_lotus.time_of_life)]
         average = sum(lifetime) / len(lifetime)
-        print(average)
-        
+        return average

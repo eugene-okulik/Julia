77a94e0ce4b7526c1368eacd7e61d47828a00659
Julia Shevkunova <shevkunova.yulia@gmail.com>
2025-07-15 11:25:17 +0200

    homework_20 is done

---
+++ homework/julia_shevkunova/homework_20/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import requests
+
+
+@pytest.fixture()
+def new_object_id():
+    body = {"name": "new", "data": {"color": "white", "size": "big"}}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(
+        'http://167.172.172.115:52353/object',
+        json=body,
+        headers=headers
+    )
+    print(response)
+    object_id = response.json()['id']
+    yield object_id
+    print('deleting the object')
+    requests.delete(f'http://167.172.172.115:52353/object/{object_id}')
+    

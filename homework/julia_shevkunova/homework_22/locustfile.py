c5f54777f57d0dd90bee5b465c5400d5eeffbf56
Julia Shevkunova <shevkunova.yulia@gmail.com>
2025-08-02 12:40:27 +0200

    homework_22

---
+++ homework/julia_shevkunova/homework_22/locustfile.py
@@ -0,0 +1,21 @@
+from locust import task, HttpUser
+import random
+
+
+class ObjectUser(HttpUser):
+
+
+    @task(1)
+    def get_all_objects(self):
+        self.client.get(
+            url='/object',
+            headers = {'Content-Type': 'application/json'}
+        )
+
+
+    @task(3)
+    def get_one_object(self):
+        self.client.get(
+            url=f'/object/{random.choice([2258, 2260, 2262, 4564])}',
+            headers={'Content-Type': 'application/json'}
+        )

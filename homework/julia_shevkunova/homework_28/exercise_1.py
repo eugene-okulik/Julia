0a238fd82bded9d2a1e2810e395a4c90b4f751a7
Julia Shevkunova <shevkunova.yulia@gmail.com>
2026-07-05 21:23:13 +0200

    homework_28

---
+++ homework/julia_shevkunova/homework_28/exercise_1.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page
+
+
+def test_get_by_role(page: Page):
+    page.goto("https://the-internet.herokuapp.com/")
+    page.get_by_role("link", name="Form Authentication").click()
+    page.get_by_role("textbox", name="Username").fill("tomsmith")
+    page.get_by_label("Password").fill("SuperSecretPassword!")
+    page.get_by_role("button", name="Login").click()
+
+
+
+
+
+
+
+
+
+

25d0b354c0d18ae28f3980e404a785581c6cb1d2
Julia Shevkunova <shevkunova.yulia@gmail.com>
2026-07-06 10:42:25 +0200

    homework_28 update

--- homework/julia_shevkunova/homework_28/exercise_1.py
+++ homework/julia_shevkunova/homework_28/exercise_1.py
@@ -5,5 +5,5 @@ def test_get_by_role(page: Page):
     page.goto("https://the-internet.herokuapp.com/")
     page.get_by_role("link", name="Form Authentication").click()
     page.get_by_role("textbox", name="Username").fill("tomsmith")
-    page.get_by_label("Password").fill("SuperSecretPassword!")
+    page.get_by_role("textbox", name="Password").fill("SuperSecretPassword!")
     page.get_by_role("button", name="Login").click()
